d8838e9cb68f8b016aa1e45d0f0b2b9dc07e2fcc
Tobias Wenck <tobias.wenck@icloud.com>
2023-08-10 05:58:28 +0200

    First Layout & Backgrounds

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { MainChatComponent } from './main-chat/main-chat.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ChatItemComponent } from './chat-item/chat-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactListComponent,
+    MainChatComponent,
+    ProfileComponent,
+    ChatItemComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/chat-item/chat-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-item',
+  templateUrl: './chat-item.component.html',
+  styleUrls: ['./chat-item.component.scss']
+})
+export class ChatItemComponent {
+
+}
