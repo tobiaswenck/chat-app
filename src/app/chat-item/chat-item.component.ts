75be1105f87ac50d223d5abf31ef78c76a23af19
Tobias Wenck <tobias.wenck@icloud.com>
2023-08-11 03:10:10 +0200

    second day

--- src/app/chat-item/chat-item.component.ts
+++ src/app/chat-item/chat-item.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-chat-item',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./chat-item.component.scss']
 })
 export class ChatItemComponent {
-
+  @Input () chatterName: String = "Richard";
+  @Input() class: string = "";
 }
--- src/app/contact-list/contact-list.component.ts
+++ src/app/contact-list/contact-list.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact-list.component.scss']
 })
 export class ContactListComponent {
+  chatItemClass: string = 'active'; // Set your desired CSS class name
 
+  
+  
 }
